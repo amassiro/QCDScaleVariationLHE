bdc77652b395421ba87318bf31d316fbd2bacfc1
Andrea <massironi.andrea@gmail.com>
2013-12-12 23:30:57 +0100

    -

--- Calculate.cxx
+++ Calculate.cxx
@@ -10,12 +10,17 @@
  std::cout << " >=0) :: " << tUp->GetEntries() << " --- " << tDo->GetEntries() << std::endl;
  std::cout << " >=1) :: " << tUp->GetEntries("jetpt1>=30") << " --- " << tDo->GetEntries("jetpt1>=30") << std::endl;
  std::cout << " >=2) :: " << tUp->GetEntries("jetpt2>=30") << " --- " << tDo->GetEntries("jetpt2>=30") << std::endl;
+ std::cout << std::endl;
 
 
  std::cout << " ==0) :: " << tCe->GetEntries() << std::endl;
  std::cout << " ==1) :: " << tCe->GetEntries("jetpt1>=30") << std::endl;
  std::cout << " ==2) :: " << tCe->GetEntries("jetpt2>=30") << std::endl;
+ std::cout << std::endl;
 
+ std::cout << " f0) :: " << 1.* tCe->GetEntries() / tCe->GetEntries() << std::endl;
+ std::cout << " f1) :: " << 1.* tCe->GetEntries("jetpt1>=30") / tCe->GetEntries() << std::endl;
+ std::cout << " f2) :: " << 1.* tCe->GetEntries("jetpt2>=30") / tCe->GetEntries() << std::endl;
  std::cout << std::endl;
 
 }
