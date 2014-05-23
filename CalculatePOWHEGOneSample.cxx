121ea3ae36467bca0087352d3cac52bb13536577
Andrea <massironi.andrea@gmail.com>
2014-05-23 10:28:48 +0200

    half error

--- CalculatePOWHEGOneSample.cxx
+++ CalculatePOWHEGOneSample.cxx
@@ -81,8 +81,8 @@
 
 
 
-  double sigma0 = 1.* abs(max_0-min_0) / c0;
-  double sigma1 = 1.* abs(max_1-min_1) / c1;
+  double sigma0 = 0.5 * abs(max_0-min_0) / c0;
+  double sigma1 = 0.5 * abs(max_1-min_1) / c1;
   double sigma2 = 0.;
 
  // k = exp (sigma)
@@ -97,7 +97,7 @@
   double t12 = pow(k2,-f2/f1);
   double t22 = k2;
 
-  std::cout << " thr = " << threshold << " c0:c1:c2 = (" << c0 << ":" << c1 << ":" << c2 << ")  :: t01 = " << t01 << " t11 = " << t11 ;
+  std::cout << " thr = " << threshold << " c0:c1:c2 = (" << c0 << ":" << c1 << ":" << c2 << ")  :: t00 = " << t00 << ":: t01 = " << t01  << " t11 = " << t11 ;
   std::cout << " :: f0 = " << f0 << " :: k0 = " << k0 << " k1 = " << k1 << " :: sigma0 = " << sigma0 << " sigma1 = " << sigma1 << " = (" << max_1 << " - " << min_1 << " ) / " << c1 << std::endl;
   Y_t00[i] = t00;
   Y_t01[i] = t01;
